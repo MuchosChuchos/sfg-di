7b236732b79284efd909b71141ee87fe138f90ff
TOly <artemchuk.tolya@gmail.com>
2021-08-13 07:13:17 +0300

    Add new controller for greeting.

--- src/main/java/guru/springframework/sfgpetclinic/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgpetclinic/SfgDiApplication.java
@@ -2,12 +2,21 @@ package guru.springframework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import guru.springframework.sfgpetclinic.controllers.MyController;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SfgDiApplication.class, args);
+		
+		MyController myController = (MyController) ctx.getBean("myController");
+		
+		String greeting = myController.sayHello();
+		
+		System.out.println(greeting);
 	}
 
 }
